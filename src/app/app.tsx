1b012f32341ac923f80a5c834bb05e9e679f0992
Ewok1ng <joker187566@gmail.com>
2025-05-12 10:49:14 +0300

    fix: partners grid layout

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -6,15 +6,15 @@ import 'virtual:svg-icons-register';
 import './styles/styles.scss';
 
 import { Main } from '@pages/main';
-import { Registration } from '@pages/registration';
+// import { Registration } from '@pages/registration';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" element={<Main />} />
-				<Route path="#directions" element={<Main />} />
-				<Route path="/registration" element={<Registration />} />
+				<Route path="/registration" element={<Main />} />{' '}
+				{/* <Registration /> */}
 			</Routes>
 		</BrowserRouter>
 	</React.StrictMode>
